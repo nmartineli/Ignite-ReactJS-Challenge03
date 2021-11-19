2c68bd98db38b3ab43a071472175057e27d437b8
Natalia Martineli <nataliamartineli@gmail.com>
2021-11-18 21:45:10 -0300

    Add to cart and cart item amount finished

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,18 +23,23 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const addedProduct = await api
+        .get(`/products/${productId}`)
+        .then((response) => response.data);
+      setCart([...cart, addedProduct]);
+
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart));
     } catch {
       // TODO
     }
